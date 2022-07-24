27c75eab72bddb74a49422afcfe9720b50687554
Agostina Vocero <agosvoce@gmail.com>
2022-07-24 20:03:07 -0300

    desafio

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log ("hola Agos")
+// console.log ("agostina")
+
+let numero = Number (prompt("numero"))
+
+for (let i=0; i <= 10; i++)  {
+    
+    alert("Hola Ferran")
+    console.log( numero)
+    
+    
+}
