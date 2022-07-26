302f5b8da59cf5b6494e657847a8e9cdac3109e3
Agostina Vocero <agosvoce@gmail.com>
2022-07-26 13:56:09 -0300

    desaf

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 // console.log ("agostina")
 
-let numero = Number (prompt("numero"))
+// calcular nota final de alumnos ingresados
 
-for (let i=0; i <= 10; i++)  {
-    
-    alert("Hola Ferran")
-    console.log( numero)
-    
-    
+ 
+
+// function promedio() {
+//     let alumno = prompt("ingrese nombre")
+
+//     let primeraNota = Number (prompt("Ingrese nota 1"))
+//     let segundaNota = Number (prompt("Ingrese nota 2"))
+//     let terceraNota = Number (prompt("Ingrese nota 3"))
+
+//     let promedioFinal = (primeraNota + segundaNota + terceraNota) / 3
+
+//     return alert (alumno + ":" + "tu promedio final es " + promedioFinal)
+// }
+
+// console.log(promedio())
+
+
+
+const promedio = () =>  {
+    let alumno = prompt("ingrese nombre")
+
+    let primeraNota = Number (prompt("Ingrese nota 1"))
+    let segundaNota = Number (prompt("Ingrese nota 2"))
+    let terceraNota = Number (prompt("Ingrese nota 3"))
+
+    let promedioFinal = (primeraNota + segundaNota + terceraNota) / 3
+    // return promedioFinal
+    return alert (alumno + ":" + "tu promedio final es " + promedioFinal)
 }
+
+console.log(promedio())
+console.log ("fin del programa")
